8533442dcb5888a94319c7bfc85b5cb739d150b7
aidarvi <aidaravaii@gmail.com>
2024-08-14 17:06:15 +0330

    feat: add environment variables

---
+++ src/config/configuration.ts
@@ -0,0 +1,10 @@
+export default () => ({
+  node_env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  postgres_host: process.env.POSTGRESS_HOST,
+  postgres_port: process.env.POSTGRESS_PORT,
+  postgres_username: process.env.POSTGRESS_USERNAME,
+  postgres_password: process.env.POSTGRESS_PASSWORD,
+  postgres_database: process.env.POSTGRESS_DATABASE,
+  log_file: process.env.LOG_FILE,
+});
