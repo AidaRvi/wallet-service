3d3b9a7e6d8333c2623e361d950894959127f7b2
aidarvi <aidaravaii@gmail.com>
2024-08-16 15:08:34 +0330

    feat: create user entity

---
+++ src/balance/entities/user.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Transaction } from './transaction.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column({ type: 'character', unique: true })
+  username: string;
+
+  @Column({ default: 0, type: 'int' })
+  balance: number;
+
+  @OneToMany(() => Transaction, (transaction) => transaction.id)
+  transactions: number[];
+
+  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
