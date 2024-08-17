845e7657e0714480c7980bbf15124842a279ba3f
aidarvi <aidaravaii@gmail.com>
2024-08-17 15:36:06 +0330

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 import { LoggingInterceptor } from './logger.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggingInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('wallet-service')
+    .setDescription('The wallet-service API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {
+    deepScanRoutes: true,
+  });
+  SwaggerModule.setup('document', app, document);
+
   const port = configService.get('PORT');
   await app.listen(port, () => {
     console.log(`wallet-service is running on port "${port}"`);
